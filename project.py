0c1f780d7cc1749a2e52b7c5d5511e1cdbb61aa0
hs4cf <59421608+hs4cf@users.noreply.github.com>
2020-03-15 14:45:33 -0400

    Update project.py
    addingExampleLinearRegression

--- project.py
+++ project.py
@@ -13,3 +13,42 @@ df[df['state'].isin([stateOfInterest])]
 #testing
 #HanimTest
 #Anvartest
+
+dataset.shape
+dataset.describe()
+dataset.plot(x='median_household_income', y='avg_hatecrimes_per_100k_fbi', style='o')  
+plt.title('median_household_income vs avg_hatecrimes_per_100k_fbi')  
+plt.xlabel('median_household_income')  
+plt.ylabel('avg_hatecrimes_per_100k_fbi')  
+plt.show()
+
+plt.figure(figsize=(15,10))
+plt.tight_layout()
+seabornInstance.distplot(dataset['avg_hatecrimes_per_100k_fbi'])
+
+X = dataset['median_household_income'].values.reshape(-1,1)
+y = dataset['avg_hatecrimes_per_100k_fbi'].values.reshape(-1,1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y,  random_state=0)
+regressor = LinearRegression()  
+regressor.fit(X_train, y_train) #training the algorithm
+#To retrieve the intercept:
+print("intercept: ")
+print(regressor.intercept_)
+#For retrieving the slope:
+print("slope: ")
+print(regressor.coef_)
+y_pred = regressor.predict(X_test)
+
+df = pd.DataFrame({'Actual': y_test.flatten(), 'Predicted': y_pred.flatten()})
+df
+
+df1 = df.head(25)
+df1.plot(kind='bar',figsize=(16,10))
+plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
+plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+#plt.show()
+
+plt.scatter(X_test, y_test,  color='gray')
+plt.plot(X_test, y_pred, color='red', linewidth=2)
+plt.show()
