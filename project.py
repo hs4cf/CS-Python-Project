478a99a5f2f68690df9836341a8ca936fbfb5a97
hs4cf <59421608+hs4cf@users.noreply.github.com>
2020-03-17 22:50:53 -0400

    Update project.py
    update after group discussion on 3/17/2020

--- project.py
+++ project.py
@@ -1,8 +1,19 @@
+import pandas as pd  
+import numpy as np  
+import matplotlib.pyplot as plt  
+import seaborn as seabornInstance 
+from sklearn.model_selection import train_test_split 
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+%matplotlib inline
 
-from numpy import *
-import pandas as pd
 
 # Read the csv file:
+url = 'https://raw.githubusercontent.com/hs4cf/CS-Python-Project/master/hate_crimes.csv'
+dataset = pd.read_csv(url, error_bad_lines=False)
+#df
+
+#dataset = pd.read_csv('hate_crimes.csv',header=0, encoding = "ISO-8859-1")
 df = pd.read_csv('hate_crimes.csv',header=0, encoding = "ISO-8859-1")
 
 #Take user input
@@ -12,14 +23,13 @@ stateOfInterest = lower_input.capitalize()
 
 df[df['state'].isin([stateOfInterest])] 
 
-
-#testing
-#HanimTest
-#Anvartest
-
 dataset.shape
 dataset.describe()
 dataset.plot(x='median_household_income', y='avg_hatecrimes_per_100k_fbi', style='o')  
+dataset.plot(x='median_household_income', y='hate_crimes_per_100k_splc', style='o')  
+dataset.plot(x='avg_hatecrimes_per_100k_fbi', y='gini_index', style='o')  
+
+
 plt.title('median_household_income vs avg_hatecrimes_per_100k_fbi')  
 plt.xlabel('median_household_income')  
 plt.ylabel('avg_hatecrimes_per_100k_fbi')  
